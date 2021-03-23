c06eaa56fa62d9af6f9c185c513fefc0491cdb92
Daniel R Gorgonha <rogergorgonha@gmail.com>
2021-03-23 10:27:08 -0300

    Creating specification repository

--- src/Modules/cars/services/CreateSpecificantionService.ts
+++ src/Modules/cars/services/CreateSpecificantionService.ts
@@ -1,8 +1,24 @@
+import { ISpecificantionsRepository } from "../repositories/ISpecificantionsRepository";
 
+interface IRequest {
+  name: string;
+  description: string;
+}
 
 class CreateSpecificantionService {
-  execute() {
-    console.log("TODO");
+  constructor(private specificationsRepository: ISpecificantionsRepository) {}
+
+  execute({ name, description }: IRequest) {
+    const specificationAlreadyExists = this.specificationsRepository.findByName(name);
+
+    if (specificationAlreadyExists) {
+      throw new Error("Specification already exists!");
+    };
+
+    this.specificationsRepository.create({
+      name,
+      description
+    });
   }
 }
 
---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { SpecificationsRepository } from "../modules/cars/repositories/SpecificantionsRepository";
+import { CreateSpecificantionService } from "../modules/cars/services/CreateSpecificantionService";
+
+const specificationsRoutes = Router();
+
+const specificationRepository = new SpecificationsRepository();
+
+specificationsRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
+  const createSpecificationService = new CreateSpecificantionService(specificationRepository);
+
+  createSpecificationService.execute({ name, description });
+
+  return response.status(201).send();
+});
+
+export { specificationsRoutes };
